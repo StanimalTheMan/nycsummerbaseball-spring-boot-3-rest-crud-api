817d40769069afa9dbfe4f3560ce8958055aab00
StanimalTheMan <sjc591@nyu.edu>
2023-05-01 22:47:48 -0400

    Initialize repo

---
+++ src/main/java/com/stanimalis/springboot/nycsummerbaseball/NycsummerbaseballApplication.java
@@ -0,0 +1,13 @@
+package com.stanimalis.springboot.nycsummerbaseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycsummerbaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NycsummerbaseballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stanimalis/springboot/nycsummerbaseball/NycsummerbaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanimalis.springboot.nycsummerbaseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NycsummerbaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
